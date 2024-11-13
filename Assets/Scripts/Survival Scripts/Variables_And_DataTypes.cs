b9353bbf06bc86c1599ddf4dd8548b324df0911b
marvin-git-repo <marvin-git-repo@gmail.com>
2024-11-13 12:16:58 +0800

    modified variables and data types script

--- Assets/Scripts/Survival Scripts/Variables_And_DataTypes.cs
+++ Assets/Scripts/Survival Scripts/Variables_And_DataTypes.cs
@@ -4,8 +4,24 @@ using UnityEngine;
 
 public class Variables_And_DataTypes : MonoBehaviour
 {
-    //Common Data Types
+    //[SerializeField] to make it private but still modify it in inspector
+    //public - to make it visible in inspector and access by other script
+    //private - only accesible inside the class
 
+    //*NAMING CONVENTION*
+    //private field - _lowerCamelCase
+    //public field - UpperCamelCase
+    //protected field - UpperCamelCase
+    //internal field - UpperCamelCase
+    //property - UpperCamelCase
+    //method - UpperCamelCase
+    //class - UpperCamelCase
+    //interface - IUpperCamelCase
+    //local variable - lowerCamelCase
+    //parameter - lowerCamelCase
+
+
+    //*COMMON DATA TYPES*
     public int score = 25;
     public float speed = 3.1415927f;
     public double gold = 3.141592653589793;
