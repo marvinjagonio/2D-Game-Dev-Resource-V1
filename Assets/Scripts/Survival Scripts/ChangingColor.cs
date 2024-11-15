7a7932e1a3b7a9975694f42481704c92365bab28
marvin-git-repo <marvin-git-repo@gmail.com>
2024-11-15 11:42:20 +0800

    added change color reference

---
+++ Assets/Scripts/Survival Scripts/ChangingColor.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangingColor : MonoBehaviour
+{
+    [SerializeField] GameObject triangle;
+    private SpriteRenderer triangleRenderer;
+
+    private int _nextColor;
+
+    private void Start()
+    {
+        triangleRenderer = GetComponent<SpriteRenderer>();
+        Debug.Log(_nextColor);
+    }
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            _nextColor = 1;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            _nextColor = 2;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            _nextColor = 3;
+        }
+
+
+        switch(_nextColor)
+        {
+            case 1:
+                triangleRenderer.color = Color.red;
+                //triangle.GetComponent<SpriteRenderer>().color = Color.red;
+                break;
+                
+        }
+
+
+    }
+}
