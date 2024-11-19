3d507fdc1093f1090a8e94e11c5b5f0715f1f70c
marvin-git-repo <marvin-git-repo@gmail.com>
2024-11-19 09:16:34 +0800

    added raycast system

---
+++ Assets/Scripts/Raycast/PlayerRaycast.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerRaycast : MonoBehaviour
+{
+    Rigidbody2D PlayerRb;
+
+    public float rayDistance = 1f;
+    public LayerMask layerMask;
+    private Vector2 playerDirection;
+
+    private void Start()
+    {
+        PlayerRb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        MovementControls();
+        PlayerRaycastDetection();
+    }
+
+    private void MovementControls()
+    {
+        if(Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(Vector2.left * Time.deltaTime * 5);
+            playerDirection = Vector2.left;
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(Vector2.right * Time.deltaTime * 5);
+            playerDirection = Vector2.right;
+        }
+    }
+
+   private void PlayerRaycastDetection()
+    {
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, playerDirection, rayDistance, layerMask);
+
+        Debug.DrawRay(transform.position, playerDirection * rayDistance, Color.red);
+
+        if (hit)
+        {
+            Debug.Log("Hit!");
+        }
+    }
+}
